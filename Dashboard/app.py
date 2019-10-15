5e661d65ec079604a26d1e4ce4207313b33f8e05
wcopeland118 <wcopeland118@gmail.com>
2019-10-14 19:17:22 -0500

    adding sqlite

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -13,16 +13,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/CleanAverageDroughtData_20102019.sqlite"
+# db = SQLAlchemy(app)
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(db.engine, reflect=True)
+
+# # Save references to each table
+# drought_data = Base.classes.CleanAverageDroughtData_20102019
+
 
 @app.route("/")
 def index():
-    """Return the homepage."""
     return render_template("index.html")
 
 
 @app.route("/data")
 def data():
-    return render_template("table.html")
+    stmt = db.session.query(drought_data).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    return jsonify(list(df.columns)[:])
 
 
 @app.route("/map")

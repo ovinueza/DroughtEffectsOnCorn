15538c9721bd02cd9afc65ea0efef0fbef38116d
oswaldvinueza <Oswald.vinueza@gmail.com>
2019-10-18 14:15:53 -0500

    updated map and images

--- Maps/DroughtLevelMap/static/js/d3.legend.js
+++ Maps/DroughtLevelMap/static/js/d3.legend.js
@@ -1,12 +1,11 @@
 
   function getColor(corn) {
-    return corn> 200000 ? "#FF0000":
-    corn>150000?"#FF8C00":
-    corn>100000?"#FFA500":
-    corn>50000?"#FFD700":
-    corn>10000? "#ADFF2F":
-    corn>0? "#7CFC00":
-    "808080"
+    return corn='D4'?"#730000":
+    corn='D3'?"#E60000":
+    corn='D2'?"#FFAA00":
+    corn='D1'? "#FCD37F":
+    corn= 'D0'? "#FFFF00":
+    "ffffff"
   };
 
 
@@ -14,16 +13,14 @@
  var legend = L.control({ position: "bottomright" });
  legend.onAdd = function() {
    var div = L.DomUtil.create("div", "info legend");
-   corn= [0, 10000, 50000, 100000, 150000, 200000],
+   corn= ['D0', 'D1', 'D2', 'D3', 'D4'],
    labels=[];
  //Loop corn values
- 
  for (var i = 0; i < corn.length; i++) {
    div.innerHTML += '<i style="background:' + getColor(corn[i] + 1) + '"></i> ' 
    + corn[i] + (corn[i + 1] ? '&ndash;' + corn[i + 1] + '<br>' : '+');
    };
    return div;
-
 
 };
 

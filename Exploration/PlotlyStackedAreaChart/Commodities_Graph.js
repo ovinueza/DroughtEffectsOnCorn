10ee774403db2d80b618ec785e1b673c2d467b48
MichaelHarper14 <50162135+MichaelHarper14@users.noreply.github.com>
2019-10-17 20:29:08 -0500

    Add files via upload

---
+++ Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
@@ -0,0 +1,28 @@
+var trace1 = {
+  x: ['Corn', 'Sugar', 'Soybeans', 'Wheat', 'Cotton', 'Livestock',
+  'Peanuts', 'Hay and forage', 'Sorghum', 'Rice', ], 
+  y: [2.34481e+03, 1.51734e+03, 1.20719e+03, 9.11530e+02, 8.33670e+02,
+    2.90500e+02, 2.31050e+02, 1.85230e+02, 1.67360e+02, 8.61900e+01,
+    ],
+  type: 'bar',
+  name: 'Subsidies',
+  marker: {
+    color: 'rgb(49,130,189)',
+    opacity: 0.7
+  }
+
+
+};
+
+
+var data = [trace1];
+
+var layout = {
+  title: 'Commodity Subsidies',
+  xaxis: {
+    tickangle: -45
+  },
+  barmode: 'group'
+};
+
+Plotly.newPlot('bar', data, layout);

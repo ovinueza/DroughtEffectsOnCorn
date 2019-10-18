ccac1ed4ba9ba511ea4e76c7cb67dd83473eba87
MichaelHarper14 <50162135+MichaelHarper14@users.noreply.github.com>
2019-10-18 12:44:13 -0500

    Add files via upload

--- Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
+++ Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
@@ -1,28 +1,36 @@
 var trace1 = {
-  x: ['Corn', 'Sugar', 'Soybeans', 'Wheat', 'Cotton', 'Livestock',
+  y: ['Corn', 'Sugar', 'Soybeans', 'Wheat', 'Cotton', 'Livestock',
   'Peanuts', 'Hay and forage', 'Sorghum', 'Rice', ], 
-  y: [2.34481e+03, 1.51734e+03, 1.20719e+03, 9.11530e+02, 8.33670e+02,
+  tickmode: 'auto',
+  x: [2.34481e+03, 1.51734e+03, 1.20719e+03, 9.11530e+02, 8.33670e+02,
     2.90500e+02, 2.31050e+02, 1.85230e+02, 1.67360e+02, 8.61900e+01,
     ],
   type: 'bar',
-  name: 'Subsidies',
+  orientation: 'h',
+  name: 'Subsidies (in Millions of $Dollars)',
   marker: {
     color: 'rgb(49,130,189)',
-    opacity: 0.7
+    opacity: 0.7,
+    tickmode: 'auto'
   }
 
 
 };
 
 
+
+
 var data = [trace1];
 
 var layout = {
-  title: 'Commodity Subsidies',
-  xaxis: {
-    tickangle: -45
+  title: 'Top 10 Commodities by Subsidies (USD in Millions)',
+  yaxis: {
+    tickangle: -36
   },
   barmode: 'group'
 };
+
+
+
 
 Plotly.newPlot('bar', data, layout);

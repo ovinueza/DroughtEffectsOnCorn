82cd06e56b2d2b29c3a0800e5c8e4e476e9fe7ca
MichaelHarper14 <50162135+MichaelHarper14@users.noreply.github.com>
2019-10-18 13:22:41 -0500

    Add files via upload

--- Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
+++ Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
@@ -1,8 +1,7 @@
 var trace1 = {
   y: ['Corn', 'Sugar', 'Soybeans', 'Wheat', 'Cotton', 'Livestock',
   'Peanuts', 'Hay and forage', 'Sorghum', 'Rice', ], 
-  tickmode: 'auto',
-  x: [2.34481e+03, 1.51734e+03, 1.20719e+03, 9.11530e+02, 8.33670e+02,
+    x: [2.34481e+03, 1.51734e+03, 1.20719e+03, 9.11530e+02, 8.33670e+02,
     2.90500e+02, 2.31050e+02, 1.85230e+02, 1.67360e+02, 8.61900e+01,
     ],
   type: 'bar',
@@ -11,7 +10,7 @@ var trace1 = {
   marker: {
     color: 'rgb(49,130,189)',
     opacity: 0.7,
-    tickmode: 'auto'
+   
   }
 
 
@@ -25,7 +24,18 @@ var data = [trace1];
 var layout = {
   title: 'Top 10 Commodities by Subsidies (USD in Millions)',
   yaxis: {
-    tickangle: -36
+    tickangle: -36,
+    autotick: false,
+    ticks: 'outside',
+    tick0: 0,
+    dtick: 0.25,
+    ticklen: 8,
+    tickwidth: 4,
+  },
+  xaxis: {
+    tickformat: '$',
+    
+
   },
   barmode: 'group'
 };

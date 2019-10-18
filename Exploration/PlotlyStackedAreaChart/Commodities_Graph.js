c5fb1e8be0bafa539be01c6a912051671cf84f29
MichaelHarper14 <50162135+MichaelHarper14@users.noreply.github.com>
2019-10-18 13:24:39 -0500

    Add files via upload

--- Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
+++ Exploration/PlotlyStackedAreaChart/Commodities_Graph.js
@@ -1,6 +1,6 @@
 var trace1 = {
   y: ['Corn', 'Sugar', 'Soybeans', 'Wheat', 'Cotton', 'Livestock',
-  'Peanuts', 'Hay and forage', 'Sorghum', 'Rice', ], 
+  'Peanuts', 'Hay & forage', 'Sorghum', 'Rice', ], 
     x: [2.34481e+03, 1.51734e+03, 1.20719e+03, 9.11530e+02, 8.33670e+02,
     2.90500e+02, 2.31050e+02, 1.85230e+02, 1.67360e+02, 8.61900e+01,
     ],
